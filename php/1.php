919c979847eaf985a08fa2adc6115127dd402b12
heiguang1234 <1814542535@qq.com>
2025-12-18 12:56:36 +0800

    18-2

--- php/1.php
+++ php/1.php
@@ -1,2 +1,36 @@
-<?php
-echo "this is php"; ?>
+<!DOCTYPE html>
+<header>
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.grid.min.css">
+    <style>
+        body {
+            width: 100%;
+            margin: 0 auto;
+        }
+        .brand{
+            background-color: #f8f9fa !important;
+        }
+        form{
+            max-width: 400px;
+            margin: 20px;
+            padding: 20px;
+        }
+        </style>
+</header>
+<body class='bg-light'>
+    <nav class="bg-white navbar">
+        <div class="text=center">
+            <a href="#" class="h1 navbar-brand brand-text">家常菜</a>
+        </div>
+        <div class="nav">
+            <ul class="nav float-right">
+                <li><a href="#" class="btn brand nav-link">点菜</a></li>
+            </ul>
+        </div>
+    </nav>
+    <?php
+    echo "this is php"; ?>
+<body>
+
+</html>
